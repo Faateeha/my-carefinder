dd3f76c45c1169341c797f811e50610f358c3d35
Adeyemi Fathia <fathiaomolara02@gmail.com>
2024-07-19 12:15:28 +0100

    updates

--- src/app/getstarted/page.tsx
+++ src/app/getstarted/page.tsx
@@ -1,12 +1,16 @@
 import SearchComponent from "@/app/search"
 import Hero from "@/app/hero"
+import {Text, Heading} from "@chakra-ui/react"
 
 export default function getStarted ()  {
     return ( <div>
         <Hero />
-        <h1>Get Started</h1>
-        <p>Get started with your project</p>
+        <div className="p-4 justify-center items-center">
+        <Heading as="h2" size="lg" >Care Finder</Heading>
+        <Text fontSize="lg">Find the nearest hospital to you</Text>
+        <Text fontSize="lg">Easily search for hospitals based on location and other criteria.</Text>
         <SearchComponent />
+        </div>
     </div>
      );
 }
